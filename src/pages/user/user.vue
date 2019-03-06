71c6a2b48483b11db189b7ce2217045b4e66fdcf
alwaysacc <alwaysacc@163.com>
2019-03-06 11:46:07 +0800

    1

--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -7,7 +7,26 @@
 
 <script>
 export default {
-  name: 'user'
+  name: 'user',
+  methods: {
+
+  },
+  onLoad: function (options) {
+    wx.getSetting({
+      success (res) {
+        console.log(res)
+        if (!res.authSetting['scope.record']) {
+          wx.getUserInfo({
+            scope: 'scope.userInfo',
+            success () {
+            // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+              wx.startRecord()
+            }
+          })
+        }
+      }
+    })
+  }
 }
 </script>
 
